ce11c578dba742e7c318b2b391379573fed05a28
Zhuoran Li <121198882+luoluobuli@users.noreply.github.com>
2024-04-03 16:28:57 -0700

    Upload pa1 files

---
+++ main.cpp
@@ -0,0 +1,179 @@
+#include "Triangle.hpp"
+#include "rasterizer.hpp"
+#include <eigen3/Eigen/Eigen>
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+constexpr double MY_PI = 3.1415926;
+
+Eigen::Matrix4f get_view_matrix(Eigen::Vector3f eye_pos)
+{
+    Eigen::Matrix4f view = Eigen::Matrix4f::Identity();
+
+    Eigen::Matrix4f translate;
+    translate << 1, 0, 0, -eye_pos[0], 0, 1, 0, -eye_pos[1], 0, 0, 1,
+        -eye_pos[2], 0, 0, 0, 1;
+
+    view = translate * view;
+
+    return view;
+}
+
+Eigen::Matrix4f get_model_matrix(float rotation_angleZ, 
+                                float rotation_angleY, 
+                                float rotation_angleX, 
+                                Eigen::Vector3f eye_pos)
+{
+    Eigen::Matrix4f model = Eigen::Matrix4f::Identity();
+
+    // TODO: Implement this function
+    // Create the model matrix for rotating the triangle around the Z axis.
+    // Then return it.
+    Eigen::Matrix4f rotateZ, rotateY, rotateX, translate1, translate2;
+    float radZ = rotation_angleZ * MY_PI / 180;
+    float radY = rotation_angleY * MY_PI / 180;
+    float radX = rotation_angleX * MY_PI / 180;
+
+    translate1 << 1, 0, 0, eye_pos[0], 0, 1, 0, eye_pos[1], 0, 0, 1,
+        eye_pos[2], 0, 0, 0, 1;
+    
+    translate2 << get_view_matrix(eye_pos);
+
+    rotateZ << std::cos(radZ), std::sin(radZ)*-1, 0, 0,
+            std::sin(radZ), std::cos(radZ), 0, 0,
+            0, 0, 1, 0,
+            0, 0, 0, 1;
+
+    rotateY << std::cos(radY), 0, std::sin(radY), 0,
+            0, 1, 0, 0, 
+            std::sin(radY)*-1, 0, std::cos(radY), 0,
+            0, 0, 0, 1;
+
+    rotateX << 1, 0, 0, 0, 
+            0, std::cos(radX), std::sin(radX)*-1, 0,
+            0, std::sin(radX), std::cos(radX), 0,
+            0, 0, 0, 1;
+    
+    model = translate2 * rotateX * rotateY * rotateZ * translate1 * model;
+    
+    return model;
+}
+
+Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio,
+                                      float zNear, float zFar)
+{
+    // Students will implement this function
+
+    Eigen::Matrix4f projection = Eigen::Matrix4f::Identity();
+
+    // TODO: Implement this function
+    // Create the projection matrix for the given parameters.
+    // Then return it.
+    float fov_rad = eye_fov + MY_PI / 180;
+    float yTop = std::tan(fov_rad/2) * std::abs(zNear);
+    float xRight = aspect_ratio * yTop;
+
+    // Perspective projection
+    Eigen::Matrix4f persp;
+    persp << zNear, 0, 0, 0, 
+            0, zNear, 0, 0,
+            0, 0, zNear+zFar, -zNear*zFar,
+            0, 0, 1, 0;
+    
+    // Orthographic projection
+    Eigen::Matrix4f ortho;
+    ortho << 1/xRight, 0, 0, 0,
+            0, 1/yTop, 0, 0,
+            0, 0, 1/(zFar-zNear), 0,
+            0, 0, 0, 1;
+    projection = ortho * persp * projection;
+
+    return projection;
+}
+
+int main(int argc, const char** argv)
+{
+    float angleX = 0;
+    float angleY = 0;
+    float angleZ = 0;
+    bool command_line = false;
+    std::string filename = "output.png";
+
+    if (argc >= 3) {
+        command_line = true;
+        angleZ = std::stof(argv[2]); // -r by default
+        if (argc == 4) {
+            filename = std::string(argv[3]);
+        }
+        else
+            return 0;
+    }
+
+    rst::rasterizer r(700, 700);
+
+    Eigen::Vector3f eye_pos = {0, 0, 5};
+
+    std::vector<Eigen::Vector3f> pos{{2, 0, -2}, {0, 2, -2}, {-2, 0, -2}};
+
+    std::vector<Eigen::Vector3i> ind{{0, 1, 2}};
+
+    auto pos_id = r.load_positions(pos);
+    auto ind_id = r.load_indices(ind);
+
+    int key = 0;
+    int frame_count = 0;
+
+    if (command_line) {
+        r.clear(rst::Buffers::Color | rst::Buffers::Depth);
+
+        r.set_model(get_model_matrix(angleZ, angleY, angleX, eye_pos));
+        r.set_view(get_view_matrix(eye_pos));
+        r.set_projection(get_projection_matrix(45, 1, 0.1, 50));
+
+        r.draw(pos_id, ind_id, rst::Primitive::Triangle);
+        cv::Mat image(700, 700, CV_32FC3, r.frame_buffer().data());
+        image.convertTo(image, CV_8UC3, 1.0f);
+
+        cv::imwrite(filename, image);
+
+        return 0;
+    }
+
+    while (key != 27) {
+        r.clear(rst::Buffers::Color | rst::Buffers::Depth);
+
+        r.set_model(get_model_matrix(angleZ, angleY, angleX, eye_pos));
+        r.set_view(get_view_matrix(eye_pos));
+        r.set_projection(get_projection_matrix(45, 1, 0.1, 50));
+
+        r.draw(pos_id, ind_id, rst::Primitive::Triangle);
+
+        cv::Mat image(700, 700, CV_32FC3, r.frame_buffer().data());
+        image.convertTo(image, CV_8UC3, 1.0f);
+        cv::imshow("image", image);
+        key = cv::waitKey(10);
+
+        // std::cout << "frame count: " << frame_count++ << '\n';
+
+        if (key == 'a') {
+            angleZ += 10;
+        }
+        else if (key == 'd') {
+            angleZ -= 10;
+        } 
+        else if (key == 'w') {
+            angleX += 10;
+        }
+        else if (key == 's') {
+            angleX -= 10;
+        }
+        else if (key == 'q') {
+            angleY += 10;
+        }
+        else if (key == 'e') {
+            angleY -= 10;
+        }
+    }
+
+    return 0;
+}
